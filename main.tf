b2703f32c57cae91fb5d3bb80356af3a88ae71ef
Tejas Jadhav <tejasjadhav.tj2002+github@gmail.com>
2025-03-26 13:59:22 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    linux = {
+      source = "TelkomIndonesia/linux"
+      version = "0.7.0"
+    }
+  }
+}
+
+provider "linux" {
+    host     = "192.168.80.129"
+    port     = 22
+    user     = "tejas"
+    password = "redhat"
+}
+
+resource "linux_directory" "resource1" {
+    path = "/tmp/terraform1"
+}
